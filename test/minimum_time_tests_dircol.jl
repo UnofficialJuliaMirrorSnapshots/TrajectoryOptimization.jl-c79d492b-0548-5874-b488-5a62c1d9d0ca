d87029f47b0af3044922acdbe29c0f7aaa8d1589
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-09 22:54:56 +0000

    Snapshot of branch master taken on 2019-07-09T18:54:50.963-04:00 from "https://github.com/UnofficialJuliaMirror/TrajectoryOptimization.jl-c79d492b-0548-5874-b488-5a62c1d9d0ca"

--- test/minimum_time_tests_dircol.jl
+++ test/minimum_time_tests_dircol.jl
@@ -27,7 +27,7 @@ opts_altro = ALTROSolverOptions{T}(verbose=false,opts_al=opts_al,R_minimum_time=
 u_bnd = 5.
 bnd = BoundConstraint(n,m,u_min=-u_bnd,u_max=u_bnd,trim=true)
 goal_con = goal_constraint(xf)
-con = [bnd, goal_con]
+con = [bnd]
 
 # problem
 N = 31
@@ -36,6 +36,7 @@ obj = LQRObjective(Q,R,Qf,xf,N)
 
 dt = 0.15
 prob = Problem(model_d,obj,U,constraints=Constraints(con,N),dt=dt,x0=x0)
+prob.constraints[N] += goal_con
 solve!(prob,opts_altro)
 tt = total_time(prob)
 
@@ -63,53 +64,30 @@ idx = [(1:n)...,((1:m) .+ n̄)...]
 @test norm(prob_mt.X[end] - xf,Inf) < 1e-3
 @test max_violation(prob_mt) < opts_al.constraint_tolerance
 
-## Box parallel park
-model = TrajectoryOptimization.Dynamics.car_model
-n = model.n; m = model.m
-model_d = discretize_model(model,:rk4)
+
+prob_mintime = minimum_time_problem()
+
+prob.constraints[1][1] isa BoundConstraint
+is_stage(prob.constraints[N][1])
 
 
-# cost
-x0 = [0.0;0.0;0.]
-xf = [0.0;1.0;0.]
-tf =  3.
-Qf = 100.0*Diagonal(I,n)
-Q = (1e-2)*Diagonal(I,n)
-R = (1e-2)*Diagonal(I,m)
 
-# options
-verbose=false
-opts_ilqr = iLQRSolverOptions{T}(verbose=false,live_plotting=:off)
-opts_al = AugmentedLagrangianSolverOptions{T}(verbose=false,opts_uncon=opts_ilqr,
-    iterations=30,penalty_scaling=10.0)
-opts_altro = ALTROSolverOptions{T}(verbose=false,opts_al=opts_al,R_minimum_time=10.0,
-    dt_max=0.2,dt_min=1.0e-3)
+opts = DIRCOLSolverOptions{T}()
+dircol = DIRCOLSolver(prob, opts)
+d = DIRCOLProblem(update_problem(prob,model=Dynamics.pendulum_model), dircol)
 
-# constraints
-u_bnd = 2.
-x_min = [-0.25; -0.001; -Inf]
-x_max = [0.25; 1.001; Inf]
-bnd = BoundConstraint(n,m,x_min=x_min,x_max=x_max,u_min=-u_bnd,u_max=u_bnd,trim=true)
+is_terminal(prob.constraints[N][1])#(zeros(2),rand(n),rand(m))
 
-goal_con = goal_constraint(xf)
-con = [bnd, goal_con]
+for k = 1:N
+    for con in prob.constraints[k]
+        if con isa BoundConstraint
+            println("success")
+        end
+    end
+end
+bounds(prob.constraints[2])
+prob
 
-# problem
-N = 51
-U = [ones(m) for k = 1:N-1]
-obj = LQRObjective(Q,R,Qf,xf,N)
+prob_c = update_problem(prob,model=Dynamics.pendulum_model)
 
-dt = 0.06
-prob = Problem(model_d,obj,U,constraints=Constraints(con,N),dt=dt,x0=x0)
-solve!(prob,opts_altro)
-tt = total_time(prob)
-
-prob_mt = Problem(model_d,obj,prob.U,constraints=Constraints(con,N),dt=dt,x0=x0,tf=:min)
-solve!(prob_mt,opts_altro)
-tt_mt = total_time(prob_mt)
-
-@test tt_mt < 0.75*tt
-@test tt_mt < 2.1
-
-@test norm(prob_mt.X[end] - xf,Inf) < 1e-3
-@test max_violation(prob_mt) < opts_al.constraint_tolerance
+minimum_time_problem(prob_c)
